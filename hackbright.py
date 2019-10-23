764fa239874358935dae114328c917f64c1c1246
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-22 17:54:33 -0700

    Completed hackbright sql

--- hackbright.py
+++ hackbright.py
@@ -21,7 +21,14 @@ def connect_to_db(app):
 
 
 def get_student_by_github(github):
-    """Given a GitHub account name, print info about the matching student."""
+    """Given a GitHub account name, print info about the matching student.
+    
+    Example:
+        >>> get_student_by_github('jhacks')
+        Student: Jane Hacker
+        GitHub account: jhacks
+
+    """
 
     QUERY = """
         SELECT first_name, last_name, github
@@ -42,23 +49,97 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+    QUERY = """
+        INSERT INTO students (first_name, last_name, github)
+            VALUES (:first_name, :last_name, :github)    
+        """
+
+    db.session.execute(QUERY, {'first_name' : first_name,
+                                'last_name' : last_name,
+                                'github' : github})
+    db.session.commit()
+    print(f"Successully added student: {first_name} {last_name}")
 
 
 def get_project_by_title(title):
-    """Given a project title, print information about the project."""
-    pass
+    """Given a project title, print information about the project.
+
+    EXAMPLE:
+        >>> get_project_by_title('Markov')
+        RESULT:
+        ID: 1
+        TITLE: Markov
+        DESCRIPTION: Tweets generated from Markov chains
+        MAX GRADE: 50
+    """
+    
+    # psuedocode: 
+    # pass in title to a query the looks up project info 
+    # WHERE project_name = title
+    # execute a query with a placeholder for the title
+    # print information
+
+    QUERY = """
+        SELECT * FROM projects
+        WHERE title = :title
+        """
+
+    cursor = db.session.execute(QUERY, {'title': title})
+
+    result = cursor.fetchone()
+
+    proj_id, title, description, max_grade = result  # unpack the resulting tuple
+
+    print('RESULT:')
+    print('ID:', proj_id)
+    print('TITLE:', title)
+    print('DESCRIPTION:', description)
+    print('MAX GRADE:', max_grade)
 
 
 def get_grade_by_github_title(github, title):
-    """Print grade student received for a project."""
-    pass
+    """Print grade student received for a project.
+    
+    EXAMPLE:
+    >>> get_grade_by_github_title('jhacks', 'Markov')
+    GRADE: 10
 
+    """
+    
+    QUERY = """
+        SELECT grade FROM grades
+        WHERE student_github = :student_github
+            AND project_title = :project_title 
+    """
+
+    cursor = db.session.execute(QUERY, {'student_github': github,
+                                        'project_title': title})
+
+    result = cursor.fetchone()[0]
+
+    print('GRADE:', result)
 
 def assign_grade(github, title, grade):
-    """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    """Assign a student a grade on an assignment and print a confirmation.
 
+    EXAMPLE:
+        >>> assign_grade('k3llymariee', 'Markov', 5)
+        Successfully added k3llymariee's grade of 5 for Markov
+
+    """
+    
+    QUERY = """
+        INSERT INTO grades (student_github, project_title, grade)
+            VALUES (:github, :title, :grade)    
+        """
+
+    db.session.execute(QUERY, {'github': github, 
+                                'title': title,
+                                'grade': grade})
+
+    db.session.commit()
+
+    print(f'Successfully added {github}\'s grade of {grade} for {title}')
 
 def handle_input():
     """Main loop.
@@ -80,7 +161,7 @@ def handle_input():
             get_student_by_github(github)
 
         elif command == "new_student":
-            first_name, last_name, github = args  # unpack!
+            first_name, last_name, github = args  # unpack! # why isnt command part of args
             make_new_student(first_name, last_name, github)
 
         else:
